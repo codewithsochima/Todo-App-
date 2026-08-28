000eea4c9d67569b81809efb975ad36ece17e0f6
Sochima <humpheryprecious07@gmail.com>
2026-08-28 21:48:51 +0100

    Initial commit: Server setup and Task schema complete

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to your Todo Application Server!");
+});
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Database connected succesfully"))
+  .catch((err) => console.log("Database connection error:", err));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
