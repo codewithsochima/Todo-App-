954cf25cf2c52751e99408aaaea50838671881c7
Sochima <humpheryprecious07@gmail.com>
2026-08-29 16:06:03 +0100

    fix: update User model import path casing

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const bcrypt = require("bcryptjs");
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User");
+const User = require("../models/user");
 
 router.get("/register", (req, res) => {
   res.render("register");
